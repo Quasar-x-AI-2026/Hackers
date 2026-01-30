8a460176570ada027a63d4ba251695a5a61956f2
its-Ashu09 <ashutoshpandey4437@gmail.com>
2026-01-31 05:03:46 +0530

    backend added

--- backend/agents/booking_tool.py
+++
@@ -1,5 +0,0 @@
-from langchain.tools import tool
-
-@tool
-def book_appointment(doctor_name: str) -> str:
-    return f"✅ Appointment booked successfully with {doctor_name}"
--- backend/schemas.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class ChatRequest(BaseModel):
-    symptoms: str
-    book: bool = False
