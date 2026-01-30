d0c575fd9d4f620718b3c9260b91cc344654bbb9
Arvind <jsrarvind12@gmail.com>
2026-01-30 17:34:24 +0530

    Added assets and components of frontend

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppContextProvider from "./context/AppContext.jsx";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
